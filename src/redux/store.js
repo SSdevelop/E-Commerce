0e1bf472a7cb216aa34d73f759642a166109c797
Srijan Srivastava <srijan.srivastav2@gmail.com>
2021-08-25 05:45:25 +0530

    added persistent storage options for redux using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
@@ -7,6 +8,8 @@ import rootReducer from './root-reducer';
 // By creating an array, we can spread the array and apply multiple middlewares in the future.
 const middleware = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
